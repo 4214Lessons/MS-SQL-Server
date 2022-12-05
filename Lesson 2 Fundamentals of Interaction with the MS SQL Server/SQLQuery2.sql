f8209d9753c1fb454df5bac9de380c1b4aaa659c
Novruzov Tural Feyruz <novruzov_t@itstep.edu.az>
2022-12-05 17:46:39 +0400

    finish 2 les

--- Lesson 2 Fundamentals of Interaction with the MS SQL Server/SQLQuery2.sql
+++ Lesson 2 Fundamentals of Interaction with the MS SQL Server/SQLQuery2.sql
@@ -1,1 +1,29 @@
 SELECT * FROM Students
+
+
+DECLARE @num1 AS int = 5
+DECLARE @num2  int = 3
+
+SELECT @num1 + @num2 AS [Sum]
+
+PRINT @num1 + @num2
+
+
+
+SELECT GETDATE() AS [DateTime]
+
+SELECT MONTH(GETDATE()) AS [Month]
+SELECT YEAR(GETDATE()) AS [Year]
+SELECT DAY(GETDATE()) AS [Day]
+
+
+--SELECT * FROM Students
+--WHERE MONTH(DateOfBirth) = 11
+
+
+-- Single line comment
+
+/*
+	Multi-line
+	comment
+*/
