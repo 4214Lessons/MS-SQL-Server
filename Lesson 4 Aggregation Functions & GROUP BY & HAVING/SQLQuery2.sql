1b9271eff9cfca2ce3666d1e62d06368ce33f9e7
Novruzov Tural Feyruz <novruzov_t@itstep.academy>
2022-12-09 16:14:15 +0400

    lesson4 part2

---
+++ Lesson 4 Aggregation Functions & GROUP BY & HAVING/SQLQuery2.sql
@@ -0,0 +1,44 @@
+-- Aggregation Functions
+
+SELECT * FROM Books
+
+
+SELECT SUM(Price) AS [TotalPrice] FROM Books
+
+
+SELECT SUM(Price) AS [TotalPrice] 
+FROM Books
+WHERE Category = 'Windows 98'
+
+
+
+SELECT COUNT(*) FROM Books
+SELECT COUNT([Date]) FROM Books
+
+
+
+SELECT * FROM Books
+WHERE Category = 'Windows 98'
+
+
+
+SELECT MAX(Price) AS MaxPrice, MIN(Price) AS MinPrice 
+FROM Books
+WHERE Category = 'Windows 98'
+
+
+
+SELECT AVG(Price) AS Average
+FROM Books
+WHERE Category = 'Windows 98'
+
+
+
+
+
+SELECT COUNT(DISTINCT Category) AS TotalUniqueCount 
+FROM Books
+
+
+
+SELECT * FROM Books
